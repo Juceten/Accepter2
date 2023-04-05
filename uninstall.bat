6072f6de87db8308c5571231b6cc7fd57ce72f62
Juceten <59311331+Juceten@users.noreply.github.com>
2023-04-05 16:17:01 -0500

    Delete uninstall.bat
    updating with a new installer that will take care of both installing and uninstalling

--- uninstall.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-if exist app\venv\ (
-    app\venv\Scripts\activate & pip uninstall -y -r app\requirements.txt & deactivate & rmdir app\venv /s /q  & pause
-) else (
-    echo venv is not installed...
-    pause
-)
